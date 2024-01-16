b37826742a69cb606cbce1660bed14497e6f09be
newprice247 <newprice247@gmail.com>
2024-01-16 16:23:40 -0500

    deleted aria-disabled from line 12-src/blocks/listGroup/children/DisabledItems.js

--- src/blocks/ui/components/list-group/children/DisabledItems.js
+++ src/blocks/ui/components/list-group/children/DisabledItems.js
@@ -9,7 +9,7 @@ const DisabledItems = () => {
 
             <div className="bd-example">
                 <ul className="list-group">
-                    <li className="list-group-item disabled" aria-disabled="true">Cras justo odio</li>
+                    <li className="list-group-item disabled">Cras justo odio</li>
                     <li className="list-group-item">Dapibus ac facilisis in</li>
                     <li className="list-group-item">Morbi leo risus</li>
                     <li className="list-group-item">Porta ac consectetur ac</li>
