d13cb263d4123498b855c6a8bd261c1042bdce0c
newprice247 <newprice247@gmail.com>
2024-01-22 07:32:40 -0500

    updated react, react-router-dom, react-dom; restructured pathing to createBrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,82 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import News from './pages/News';
+import NewsSinglePost from './pages/NewsSinglePost';
+import Gallery from './pages/Gallery';
+import SearchResults from './pages/SearchResults';
+import Services from './pages/Services';
+import ServicesInside from './pages/ServicesInside';
+import page404 from './pages/404';
+import UserInterface from './pages/UserInterface';
 
 import './assets/sass/style.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <page404 />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/about-us',
+                element: <About />
+            },
+            {
+                path: '/contacts',
+                element: <Contacts />
+            },
+            {
+                path: '/news',
+                element: <News />
+            },
+            {
+                path: '/gallery',
+                element: <Gallery />
+            },
+            {
+                path: '/news-single-post',
+                element: <NewsSinglePost />
+            },
+            {
+                path: '/search-results',
+                element: <SearchResults />
+            },
+            {
+                path: '/services',
+                element: <Services />
+            },
+            {
+                path: '/services-inside',
+                element: <ServicesInside />
+            },
+            {
+                path: '/user-interface',
+                element: <UserInterface />
+            }
+        ]
+    }
+])
+
+
+// ReactDOM.render( <App />, document.getElementById( 'root' ) );
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router}>
+            <App />
+        </RouterProvider>
+    </React.StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
         <Fragment>
             <MetaTags>
                 <meta charSet="UTF-8" />
-                <title>Home | Mykery - Beauty Salon React JS Template</title>
+                <title>Home | Sutton Place Hair Salon</title>
 
                 <meta httpEquiv="x-ua-compatible" content="ie=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
