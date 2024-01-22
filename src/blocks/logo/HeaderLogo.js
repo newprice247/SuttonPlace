7d90e3c11153b9eb1d6baf2713add8086496dc77
newprice247 <newprice247@gmail.com>
2024-01-22 11:30:26 -0500

    comment out unusused imports

--- src/blocks/logo/HeaderLogo.js
+++ src/blocks/logo/HeaderLogo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const HeaderLogo = ( {  } ) => {
+const HeaderLogo = () => {
     return (
         <div className="header-logo">
             <a className="logo logo-secondary transform-scale-h" title="Logo" href={ process.env.PUBLIC_URL + "/" }>
