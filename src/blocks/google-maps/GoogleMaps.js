797c8691265b5797de1a7d3b50b2ed63762e4b29
newprice247 <newprice247@gmail.com>
2024-02-01 08:27:43 -0500

    add styling

--- src/blocks/google-maps/GoogleMaps.js
+++ src/blocks/google-maps/GoogleMaps.js
@@ -9,7 +9,7 @@ return (
     >
      <iframe
       src={`https://storage.googleapis.com/maps-solutions-${process.env.REACT_APP_MAPS_API_KEY}/locator-plus/f7b3/locator-plus.html`}
-      className='h-100 w-100 border-0'
+      className='h-100 w-100 border rounded border-primary'
       loading="lazy"
       title="Locator Plus Map"
     ></iframe>
