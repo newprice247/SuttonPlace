d205e37f2df0a9370ea80c3d132ae05e9f3892d5
newprice247 <newprice247@gmail.com>
2024-04-30 15:50:33 -0400

    add new hrefs to cash and check logos in footer

--- src/blocks/footer/FooterMenu.js
+++ src/blocks/footer/FooterMenu.js
@@ -8,7 +8,7 @@ const FooterMenu = () => {
                 { dataFooterMenu && dataFooterMenu.map( ( item, key ) => {
                     return (
                         <li key={ key } className="nav-item">
-                            <a title={ item.title } href={ item.link }>
+                            <a title={ item.title } href={ item.link } target="_blank">
                                 <img src={ item.icon } alt={ item.title } 
                                 style={ { width: '3em', height: '3em' } }
                                 />
